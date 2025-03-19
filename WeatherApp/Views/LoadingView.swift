c052ea52ff7444b8333b08be182448ab1dc207a0
gdev-kingx <marioplayz975@gmail.com>
2025-03-19 12:36:51 +0530

    Init

--- WeatherApp/Views/LoadingView.swift
+++ WeatherApp/Views/LoadingView.swift
@@ -1,18 +1,15 @@
-//
-//  LoadingView.swift
-//  WeatherApp
-//
-//  Created by Sarveshvarn on 3/19/25.
-//
-
 import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
-#Preview {
-    LoadingView()
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
 }
--- WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/Views/WelcomeView.swift
@@ -1,10 +1,3 @@
-//
-//  WelcomeView.swift
-//  WeatherApp
-//
-//  Created by Stephanie Diep on 2021-11-30.
-//
-
 import SwiftUI
 import CoreLocationUI
 
